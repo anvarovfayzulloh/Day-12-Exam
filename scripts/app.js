3449917393d219d7d057fcfd1fa3c3f1a25be592
anvarovfayzulloh <fayzee200708@gmail.com>
2024-07-26 00:01:58 +0300

    finish

--- scripts/app.js
+++ scripts/app.js
@@ -3,12 +3,14 @@ const $blog = document.querySelector(".blog");
 
 function hideSpinner() {
     document.getElementById('spinner').classList.add('hidden');
+    document.getElementById('spinnerWRapper').classList.remove('flex')
     document.getElementById('spinnerWRapper').classList.add('hidden')
     document.getElementById('load').classList.add('hidden')
 }
 function showSpinner() {
     document.getElementById('spinner').classList.remove('hidden');
     document.getElementById('spinnerWRapper').classList.remove('hidden')
+    document.getElementById('spinnerWRapper').classList.add('flex')
     document.getElementById('load').classList.remove('hidden')
 }
 
